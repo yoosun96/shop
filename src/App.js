1219ca134040ddd140d212833d576f3921901344
yoosun96 <yoosnu@naver.com>
2021-07-19 21:45:43 +0900

    add function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
 
 function App() {
-  let [shoes, shoes변경] = useState([]);
+  let [shoes, shoes변경] = useState(Data);
 
   return (
     <div className='App'>
@@ -26,35 +29,33 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
       <div className='background'>
         <h1>20% Season Off</h1>
         <p>This is a simple hero unit</p>
         <p>
           <Button variant='primary'>Learn more</Button>
         </p>
-        {/* <img src={image} alt='background' /> */}
       </div>
 
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%' />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='100%' />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='100%' />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width='100%' />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
     </div>
   );
 }
