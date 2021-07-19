19b7267f61901a4b31ec0662a935494630f67e58
yoosnu96 <yoosnu@naver.com>
2021-07-19 19:43:34 +0900

    add commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  let [shoes, shoes변경] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>UooShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#link'>Link</Nav.Link>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className='background'>
+        <h1>20% Season Off</h1>
+        <p>This is a simple hero unit</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Button variant='primary'>Learn more</Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <img src={image} alt='background' /> */}
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%' />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='100%' />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='100%' />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
